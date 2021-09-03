faebae6e1099dddddeb28fd9aab46eb29b6830ee
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-09-03 13:40:16 +0530

    working with node.js and npm , installing libraries from npm

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+// var superheroes=require("superheroes");
+// var first=superheroes.random();
+// console.log(first);
+
+var supervillains=require("supervillains");
+var name=supervillains.random();
+console.log(name);
